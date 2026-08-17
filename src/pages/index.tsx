bd959237d8dbf12ad23dcb040ec15f8a2386d997
Илья Шубин <shubin152@gmail.com>
2026-08-17 12:25:00 +0500

    накидал заготовку главной страницы

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>SAVEUR — бронирование столика</h1>
+      <p>Тут будет форма бронирования</p>
+    </main>
+  );
+}
